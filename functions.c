6dbcf518e4e266b3ef9d82897bb94f9d21844302
Wesaal <WesaalEbraheem@hotmail.com>
2026-04-13 20:35:29 +0300

    Update functions.c
    feat: add print_number function to handle integer output

--- functions.c
+++ functions.c
@@ -16,3 +16,22 @@ int print_string(char *str)
 		count += _putchar(*str++);
 	return (count);
 }
+
+int print_number(int n)
+{
+	int count;
+	unsigned int num;
+
+	count = 0;
+	if (n < 0)
+	{
+		count += _putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+	if (num / 10)
+		count += print_number(num / 10);
+	count += _putchar((num % 10) + '0');
+	return (count);
+}
