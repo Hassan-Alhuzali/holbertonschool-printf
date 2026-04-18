685aab0d77e96e9dd574e3df2c3fa6864717c35e
Hassan-Ahuzali <hassan407@proton.me>
2026-04-19 02:30:50 +0300

    feat: add define flags to main.h

--- main.h
+++ main.h
@@ -4,6 +4,10 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+#define FLAG_PLUS   (1 << 0)
+#define FLAG_SPACE  (1 << 1)
+#define FLAG_HASH   (1 << 2)
+
 extern char buffer[1024];
 extern int buffer_index;
 
@@ -13,9 +17,9 @@ int _putchar_buffer(char c);
 void flush_buffer(void);
 int print_string(char *str);
 int print_string_nonprintable(char *str);
-int print_number(int n);
-int print_unsigned(unsigned int n, char *digits, int base);
-int print_binary(unsigned int n);
+int print_number(int n, int flags);
+int print_unsigned(unsigned int n, char *digits, int base, int flags);
+int print_binary(unsigned int n, int flags);
 int print_pointer(void *ptr);
 
 #endif /* MAIN_H */
