5d2bd1870701b2e95f9be5207846e04cf7fae586
Wesaal <WesaalEbraheem@hotmail.com>
2026-04-18 17:19:54 +0300

    Update printf.c
    feat: add local buffer to reduce write calls in _printf

--- printf.c
+++ printf.c
@@ -1,11 +1,35 @@
 #include "main.h"
 
+static char buffer[1024];
+static int buffer_index;
+
+void flush_buffer(void)
+{
+	if (buffer_index > 0)
+	{
+		write(1, buffer, buffer_index);
+		buffer_index = 0;
+	}
+}
+
+int _putchar_buffer(char c)
+{
+	buffer[buffer_index++] = c;
+	if (buffer_index == 1024)
+	{
+		write(1, buffer, buffer_index);
+		buffer_index = 0;
+	}
+	return (1);
+}
+
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count;
 
 	count = 0;
+	buffer_index = 0;
 	if (!format)
 		return (-1);
 	va_start(args, format);
@@ -15,39 +39,43 @@ int _printf(const char *format, ...)
 		{
 			format++;
 			if (*format == '\0')
+			{
+				flush_buffer();
 				return (-1);
+			}
 			if (*format == 'c')
-				count += _putchar(va_arg(args, int));
+				count += _putchar_buffer(va_arg(args, int));
 			else if (*format == 's')
 				count += print_string(va_arg(args, char *));
 			else if (*format == 'd' || *format == 'i')
 				count += print_number(va_arg(args, int));
 			else if (*format == 'u')
-				count += print_unsigned(va_arg(args, unsigned int),
-				"0123456789", 10);
+				count += print_unsigned(va_arg(args,
+				unsigned int), "0123456789", 10);
 			else if (*format == 'o')
-				count += print_unsigned(va_arg(args, unsigned int),
-				"01234567", 8);
+				count += print_unsigned(va_arg(args,
+				unsigned int), "01234567", 8);
 			else if (*format == 'x')
-				count += print_unsigned(va_arg(args, unsigned int),
-				"0123456789abcdef", 16);
+				count += print_unsigned(va_arg(args,
+				unsigned int), "0123456789abcdef", 16);
 			else if (*format == 'X')
-				count += print_unsigned(va_arg(args, unsigned int),
-				"0123456789ABCDEF", 16);
+				count += print_unsigned(va_arg(args,
+				unsigned int), "0123456789ABCDEF", 16);
 			else if (*format == 'b')
 				count += print_binary(va_arg(args, unsigned int));
 			else if (*format == '%')
-				count += _putchar('%');
+				count += _putchar_buffer('%');
 			else
 			{
-				count += _putchar('%');
-				count += _putchar(*format);
+				count += _putchar_buffer('%');
+				count += _putchar_buffer(*format);
 			}
 		}
 		else
-			count += _putchar(*format);
+			count += _putchar_buffer(*format);
 		format++;
 	}
 	va_end(args);
+	flush_buffer();
 	return (count);
 }
