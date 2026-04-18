ef4184bdc92f8c7c938578a7b4af4a361aab9dd7
Wesaal <WesaalEbraheem@hotmail.com>
2026-04-18 17:07:23 +0300

    Update main.h
    fix: correct print_number prototype in main.h

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_string(char *str);
-int print_number(va_list args); /* Changed: now takes va_list */
+int print_number(int n);
 int print_binary(unsigned int n);
 
 #endif /* MAIN_H */
