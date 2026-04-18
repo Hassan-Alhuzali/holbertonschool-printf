6743da95c6b8562fb606955763468a2aa9649dab
Wesaal <WesaalEbraheem@hotmail.com>
2026-04-18 17:07:57 +0300

    Update functions.c
    fix: correct print_number and add print_binary in functions.c

--- functions.c
+++ functions.c
@@ -16,34 +16,24 @@ int print_string(char *str)
 		count += _putchar(*str++);
 	return (count);
 }
-int print_number_recursive(int n);
-int print_number(va_list args) /* Changed: takes va_list */
-{
-  int n = va_arg(args, int); /* Get the number from args */
-  int count;
-  unsigned int num;
 
-  count = 0;
-  if (n < 0) {
-    count += _putchar('-');
-    num = -n;
-  } else
-    num = n;
-  /* Recursively print each digit */
-  if (num / 10)
-    count += print_number_recursive(num / 10);
-  count += _putchar((num % 10) + '0');
-  return (count);
-}
-/* Helper function for recursion */
-int print_number_recursive(int n)
+int print_number(int n)
 {
-	int count = 0;
+	int count;
+	unsigned int num;
 
-	if (n/ 10)
-          count += print_number_recursive(n / 10);
-        count += _putchar((n % 10) + '0');
-		return (count);
+	count = 0;
+	if (n < 0)
+	{
+		count += _putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+	if (num / 10)
+		count += print_number(num / 10);
+	count += _putchar((num % 10) + '0');
+	return (count);
 }
 
 int print_binary(unsigned int n)
