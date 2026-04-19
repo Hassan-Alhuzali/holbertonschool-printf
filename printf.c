e08d470e0c4faf6cae18f8dc00275dea58e42421
Hassan-Ahuzali <hassan407@proton.me>
2026-04-19 03:48:34 +0300

    fix: updating flags in printf.c

--- printf.c
+++ printf.c
@@ -42,23 +42,23 @@ int _printf(const char *format, ...)
 			else if (*format == 'S')
                 count += print_string_nonprintable(va_arg(args, char *));
             else if (*format == 'd' || *format == 'i')
-				count += print_number(va_arg(args, int));
+				count += print_number(va_arg(args, int), flags);
 			else if (*format == 'u')
 				count += print_unsigned(va_arg(args,
-				unsigned int), "0123456789", 10);
+				unsigned int), "0123456789", 10, flags);
 			else if (*format == 'o')
 				count += print_unsigned(va_arg(args,
-				unsigned int), "01234567", 8);
+				unsigned int), "01234567", 8, flags);
 			else if (*format == 'x')
 				count += print_unsigned(va_arg(args,
-				unsigned int), "0123456789abcdef", 16);
+				unsigned int), "0123456789abcdef", 16, flags);
 			else if (*format == 'X')
 				count += print_unsigned(va_arg(args,
-				unsigned int), "0123456789ABCDEF", 16);
+				unsigned int), "0123456789ABCDEF", 16, flags);
             else if (*format == 'p')
                 count += print_pointer(va_arg(args, void *));
 			else if (*format == 'b')
-				count += print_binary(va_arg(args, unsigned int));
+				count += print_binary(va_arg(args, unsigned int), flags);
 			else if (*format == '%')
 				count += _putchar_buffer('%');
 			else
